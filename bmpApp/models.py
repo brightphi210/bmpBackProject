28d1f3726503d51557b3454a0adf5ec6f27d4f4f
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-02-12 23:54:28 -0800

    a

--- bmpApp/models.py
+++ bmpApp/models.py
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255, null=True, blank=True)
 
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
